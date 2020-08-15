796ff02e234cb4a7c96aa68bc453ffcfe856ade9
najuspy <sazeal.sujan@gmail.com>
2020-08-15 20:14:21 +0545

    Updates

---
+++ precache-manifest.180a6b6b5d3af7c3d5cf04692279c11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01381039dadf9ac81f7a1e70ba117aec",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "09b9ceba16333f8cf980",
+    "url": "/todo-list/static/css/main.69ae0492.chunk.css"
+  },
+  {
+    "revision": "69eafd7e31536ade6092",
+    "url": "/todo-list/static/js/2.2310b1dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.2310b1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b9ceba16333f8cf980",
+    "url": "/todo-list/static/js/main.847b0631.chunk.js"
+  },
+  {
+    "revision": "5d19d48c195ae4bc65b0",
+    "url": "/todo-list/static/js/runtime-main.2c75e7f3.js"
+  }
+]);
